63f480dda1bd9a8681db8bfc1f8237422a89bd02
LaelapsV2 <nd60xw@student.aau.dk>
2024-11-13 10:21:41 +0100

    Pdf reader changed to use a library instead

--- server/moodle/blocks/homework/block_homework.php
+++ server/moodle/blocks/homework/block_homework.php
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+// require for the pdf reader
+require_once(__DIR__ .'/pdf_reader.php');
+
 /**
  * Block definition class for the block_homework plugin.
  *
@@ -23,25 +26,59 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Checks for Moodle environment
+// Checks for Moodle environment.
 defined('MOODLE_INTERNAL') || die();
 
-
+/**
+ * Class that creates the homework block
+ * @copyright group 1
+ * @package block_homework
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class block_homework extends block_base {
-
-    //constructor for the block
+    /**
+     * Constructor for the block
+     * @return void Saves the title as the correct title and nothing else
+     * @throws coding_exception Moodle standard exception if error
+     */
     public function init() {
         $this->title = get_string('homework', 'block_homework');
     }
 
     /**
      * Retrieves and prepares the content to be displayed by the block
+     *
+     * @return stdClass|null
      */
     public function get_content() {
 
-        global $OUTPUT, $PAGE, $DB;
+        global $OUTPUT, $DB, $USER;
 
+        // Get current time.
+        $currenttime = time();
+
+        // Fetch courses user is enrolled in.
+        $usercourses = enrol_get_users_courses($USER->id);
+
+        // Extract course IDs.
+        $courseids = array_map(function($course) {
+            return $course->id;
+        }, $usercourses);
+
+
+        // Create a string of ? placeholders for each found course_id, seperated by commas.
+        $placeholders = implode(',', array_fill(0, count($courseids), '?'));
+
+        // Merge parameters.
+        $parameters = array_merge([$currenttime], $courseids);
+
+        // Construct WHERE condition for select.
+        $select = "duedate > ? AND course IN ($placeholders)";
+
+        // Fetch homeworks using get_records_select.
         $homeworks = $DB->get_records('homework');
+
+
         $data = [];
 
         if ($this->content !== null) {
@@ -50,31 +87,39 @@ class block_homework extends block_base {
 
         $this->content = new stdClass();
 
-        //If the current page is a course then remove unrelated homework
-        if ($PAGE->pagetype == 'course-view-topics') {
-            $homeworks = $this->filter_homework_content($PAGE->url, $homeworks);
+        // If the current page is a course then remove unrelated homework.
+        if ($this->page->pagetype == 'course-view-topics') {
+            $homeworks = $this->filter_homework_content($this->page->url, $homeworks);
         }
 
-        foreach($homeworks as $homework) {
+        // Adding the details of each homework module to an associative array that will be pushed to the data array.
+        foreach ($homeworks as $homework) {
+
             $tmp = [];
+            $tmp['id'] = $homework->id;
             $tmp['name'] = $homework->name;
-            $tmp['duedate'] = $homework->duedate;
+            $tmp['duedate'] = date('d-m-Y', $homework->duedate);
             $tmp['intro'] = strip_tags($homework->intro);
             $tmp['courseTitle'] = $DB->get_field('course', 'fullname', ['id' => $homework->course]);
 
+
+
             $files = [];
 
-            //Get ids of homeworkfiles
+            // Get ids of homeworkfiles.
             $fileids = [];
-            $homeworkfiles = $DB->get_records('files_homework', ['files_id'=>$homework->id]);
+            $homeworkfiles = $DB->get_records('files_homework', ['homework_id' => $homework->id]);
             foreach ($homeworkfiles as $homeworkfile) {
                 array_push($fileids, $homeworkfile->files_id);
             }
 
-            //Get file records
-            if(!empty($fileids)) {
-                $file_records = $DB->get_records_list('files', 'id', $fileids);
-                foreach ($file_records as $file) {
+            // Get file records.
+            if (!empty($fileids)) {
+                $filerecords = $DB->get_records_list('files', 'id', $fileids);
+
+                $fs  = get_file_storage();
+
+                foreach ($filerecords as $file) {
                     $contextid = $file->contextid;
                     $component = $file->component;
                     $filearea = $file->filearea;
@@ -82,22 +127,19 @@ class block_homework extends block_base {
                     $filepath = $file->filepath;
                     $filename = $file->filename;
 
-                    //Generate url
+                    // Generate url.
                     $url = moodle_url::make_pluginfile_url(
                         $contextid,
                         $component,
                         $filearea,
                         $itemid,
                         $filepath,
-                        $filename
+                        $filename,
+                        false
                     );
 
-                    //Get appropriate icon for file type
+                    // Get appropriate icon for file type.
                     $iconurl = $OUTPUT->image_url(file_mimetype_icon($file->mimetype));
-
-                    // Initialize page count as null
-                    $pagecount = null;
-
 
                     // Initialize time estimate as null
                     $timeestimate = null;
@@ -105,15 +147,15 @@ class block_homework extends block_base {
                     // Initialize average words read per minute
                     $averagewordsperminute = 220;
 
+                    $file = $fs->get_file($contextid, $component, $filearea, $itemid, $filepath, $filename);
+
                     // Check file type and get page count if it's a PDF or DOCX
                     if (str_ends_with(strtolower($filename), '.pdf')) {
-                        $pdfpath = $file->get_filepath() . $filename;
-
                         //  Initialize word count reader
-                        $algorithm = new pdf_word_count_reader();
+                        $algorithm = new pdf_reader();
 
                         // Use word reading algorithm and save the value in wordcount
-                        $wordcount = $algorithm->count_words_in_pdf($pdfpath);
+                        $wordcount = $algorithm->countwordsinpdf($file);
 
                         // Calculate the time estimate based on word count and average words per minute
                         $timeestimate = $wordcount / $averagewordsperminute;
@@ -135,28 +177,32 @@ class block_homework extends block_base {
         }
 
 
-        // Render the content using a template and pass the homework data to it
+        // Render the content using a template and pass the homework data to it.
         $this->content->text = $OUTPUT->render_from_template('block_homework/data', ['data' => $data]);
-
-        // Include JavaScript functionality for scrolling behavior in the block
-        $PAGE->requires->js_call_amd('block_homework/scroll', 'init');
+        // Include JavaScript functionality for scrolling behavior in the block.
+        $this->page->requires->js_call_amd('block_homework/scroll', 'init');
 
         return $this->content;
     }
 
-    public static function filter_homework_content($URL,$homeworks): array
-    {
-        //Use a regex to remove everything but digits from the url
-        $courseid = preg_replace('/\D/', '',$URL);
-        $tmpHomeworks = [];
+    /**
+     * Filter the URL
+     * @param $url
+     * @param $homeworks
+     * @return array
+     */
+    public static function filter_homework_content($url, $homeworks): array {
+        // Use a regex to remove everything but digits from the url.
+        $courseid = preg_replace('/\D/', '', $url);
+        $tmphomeworks = [];
 
-        //Check each homework to see if the course matches the id
+        // Check each homework to see if the course matches the id.
         foreach ($homeworks as $homework) {
-            if($courseid == $homework->course){
-                array_push($tmpHomeworks, $homework);
+            if ($courseid == $homework->course) {
+                array_push($tmphomeworks, $homework);
             }
         }
-        return $tmpHomeworks;
+        return $tmphomeworks;
     }
 
 
@@ -172,5 +218,4 @@ class block_homework extends block_base {
             'my' => true,
         ];
     }
-
 }
